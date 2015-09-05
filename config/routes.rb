70b9958c00a2b0f8a91d081e931cbd1b184ab4dc
hamholla <hamhollett@gmail.com>
2015-09-05 14:17:35 -0400

    added devise, fixed dropdown links, styled login/registration pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root "movies#index"
+
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :movies
   resources :genres
   # The priority is based upon order of creation: first created -> highest priority.
