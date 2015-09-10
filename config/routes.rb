1556694ff1422895dd090c9c03fc6c4c0d086f82
hamholla <hamhollett@gmail.com>
2015-09-10 11:06:28 -0400

    put movies in generes and added movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :movies
   resources :genres
-  root "movies#index"
+  root "genres#index"
 
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :genres
